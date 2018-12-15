154e03ac03ce46836130b3878533382a40764aa8
Soham Govande <vgsoham@gmail.com>
2018-12-14 22:14:19 -0600

    Changed formatting of options buttons

--- src/select-drill-screen.js
+++ src/select-drill-screen.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 
 import './semantic/dist/semantic.min.css';
-import { Button, List, Icon, Transition, Form, Message, Segment, Popup } from 'semantic-ui-react';
+import { Button, Checkbox, Icon, Form, Placeholder } from 'semantic-ui-react';
 
 import cards from './cards';
 import CardsSelectorList from './card-selector-list';
@@ -12,9 +12,6 @@ class SelectDrillScreen extends Component {
   constructor(props) {
     super(props);
 
-    this.toggleReadingDirection = this.toggleReadingDirection.bind(this);
-    this.toggleHighlights = this.toggleHighlights.bind(this);
-    this.toggleShuffle = this.toggleShuffle.bind(this);
     this.renderHighlightsButton = this.renderHighlightsButton.bind(this);
     this.renderReadingDirectionButton = this.renderReadingDirectionButton.bind(this);
     this.moveToNextScreen = this.moveToNextScreen.bind(this);
@@ -31,6 +28,10 @@ class SelectDrillScreen extends Component {
       elements: []
     };
   }
+
+  toggleHighlights = (e, { value }) => this.setState({ highlight: !this.state.highlight })
+  toggleReadForward = (e, { value }) => this.setState({ readForward: !this.state.readForward })
+  toggleShuffle = (e, { value }) => this.setState({ shuffle: !this.state.shuffle })
 
   UNSAFE_componentWillMount() {
     const fs = window.bypass.fs;
@@ -55,43 +56,9 @@ class SelectDrillScreen extends Component {
     );
   }
 
-  toggleReadingDirection(e) {
-    e.preventDefault();
-    this.setState({
-      readForward: !this.state.readForward
-    });
-  }
-  
-  toggleHighlights(e) {
-    e.preventDefault();
-    this.setState({
-      highlight: !this.state.highlight
-    });
-  }
-
-  toggleShuffle(e) {
-    e.preventDefault();
-    this.setState({
-      shuffle: !this.state.shuffle
-    });
-  }
-
   setElementsCallback(elements) {
     console.log(elements);
     this.setState({ elements: elements });
-  }
-
-  renderHighlightsButton() {
-    const color = this.state.highlight ? 'yellow' : 'grey';
-    return (
-      <Button
-          color={color}
-          content='Highlights'
-          icon='align center'
-          onClick={this.toggleHighlights}
-          label={{ basic: true, pointing: 'left', content: this.state.highlight ? 'On':'Off' }}
-      />
-    );
   }
 
   moveToNextScreen(e) {
@@ -115,29 +82,27 @@ class SelectDrillScreen extends Component {
     });
   }
 
-  renderReadingDirectionButton() {
-    const icon = this.state.readForward ? 'sort amount down' : 'sort amount up';
+  renderHighlightsButton() {
     return (
-      <Button
-          color='black'
-          content='Reading Direction'
-          icon={icon}
-          onClick={this.toggleReadingDirection}
-          label={{ basic: true, pointing: 'left', content: this.state.readForward ? 'Forward':'Backward' }}
-      />
+      <Form.Field>
+        <Checkbox label='Highlight' onClick={this.toggleHighlights} checked={this.state.highlight}/>
+      </Form.Field>
+    );
+  }
+
+  renderReadingDirectionButton() {
+    return (
+      <Form.Field>
+        <Checkbox label='Read backward' onClick={this.toggleReadForward} checked={this.state.readForward}/>
+      </Form.Field>
     );
   }
 
   renderShuffleButton() {
-    const icon = this.state.shuffle ? 'shuffle' : 'ban';
     return (
-      <Button 
-        color='olive'
-        content='Shuffle'
-        icon={icon}
-        onClick={this.toggleShuffle}
-        label={{ basic: true, pointing: 'left', content: this.state.shuffle ? 'On':'Off' }}
-      />
+      <Form.Field>
+        <Checkbox label='Shuffle order' onClick={this.toggleShuffle} checked={this.state.shuffle}/>
+      </Form.Field>
     );
   }
 
@@ -147,7 +112,7 @@ class SelectDrillScreen extends Component {
       <h1 className="ui header">Options</h1>
 
       <Form>
-        
+      
         {this.renderHighlightsButton()}
         {this.renderReadingDirectionButton()}
         {this.renderShuffleButton()}
